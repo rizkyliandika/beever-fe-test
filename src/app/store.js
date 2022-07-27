0c3f7b4e9040d687e15132973576fc7ade24e04b
Dika <rizkyliandika@gmail.com>
2022-07-27 22:46:18 +0700

    [CHORES] remove unsused import

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import counterReducer from '../features/counter/counterSlice';
 import quotesReducer from '../features/quotes/qoutesSlice';
 
 export const store = configureStore({
   reducer: {
-    // counter: counterReducer,
     quote: quotesReducer
   },
 });
